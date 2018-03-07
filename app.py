f0e410af310bf082686f3c026ec5c021adbf4c4c
Ivan Grokhotkov <ivan@espressif.com>
2018-03-08 02:12:48 +0800

    process push events, check signature

--- app.py
+++ app.py
@@ -1,17 +1,50 @@
-from flask import Flask
+from flask import Flask, request, abort
 from datetime import datetime
+from hashlib import sha1
+import hmac
+import binascii
+import re
+from os import environ
+
 app = Flask(__name__)
 
-@app.route('/')
-def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+def check_signature(data, signature_received):
+    data = request.data
+    key = environ['GITHUB_WEBHOOK_KEY']
+    key_bytes = bytes(key, 'UTF-8')
+    hashed = hmac.new(key_bytes, data, sha1)
+    signature_expected = str(binascii.hexlify(hashed.digest()), 'UTF-8')
+    return signature_received == signature_expected
 
-    return """
-    <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
+def start_ci(repo_url, commit_id):
+    print('Starting CI for repo {} commit {}'.format(repo_url, commit_id))
 
-    <img src="http://loremflickr.com/600/400" />
-    """.format(time=the_time)
+def process_push_event(event):
+    if not event['created']:
+        return
+    if event['ref'] != 'refs/heads/master':
+        return
+    repo_url = event['repository']['clone_url']
+    commit_id = event['after']
+    start_ci(repo_url, commit_id)
+
+@app.route('/hook', methods = ['POST'])
+def hook():
+    signature_header = request.headers['x-hub-signature']
+    signature = re.search(r'sha1=([0-9A-Fa-f]+)', signature_header).group(1)
+    if not check_signature(request.data, signature):
+        abort(401)
+    
+    event = request.headers['x-github-event']
+    if event == 'push':
+        process_push_event(request.get_json())
+    else:
+        abort(400)
+
+    return 'Ok\n'
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
