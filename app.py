a272470470ac06c7433b1a8c43792060b26bdfc6
Ivan Grokhotkov <ivan@espressif.com>
2018-03-08 02:20:48 +0800

    add more logs

--- app.py
+++ app.py
@@ -11,10 +11,14 @@ app = Flask(__name__)
 def check_signature(data, signature_received):
     data = request.data
     key = environ['GITHUB_WEBHOOK_KEY']
+    print('Using key={}'.format(key))
     key_bytes = bytes(key, 'UTF-8')
     hashed = hmac.new(key_bytes, data, sha1)
     signature_expected = str(binascii.hexlify(hashed.digest()), 'UTF-8')
-    return signature_received == signature_expected
+    if signature_received != signature_expected:
+        print('Signature expected={} received={}'.format(signature_expected, signature_received))
+        return False
+    return True
 
 def start_ci(repo_url, commit_id):
     print('Starting CI for repo {} commit {}'.format(repo_url, commit_id))
@@ -33,12 +37,14 @@ def hook():
     signature_header = request.headers['x-hub-signature']
     signature = re.search(r'sha1=([0-9A-Fa-f]+)', signature_header).group(1)
     if not check_signature(request.data, signature):
+        print('Signature mismatch, ignoring request')
         abort(401)
     
     event = request.headers['x-github-event']
     if event == 'push':
         process_push_event(request.get_json())
     else:
+        print('Unsupported event, ignoring request')
         abort(400)
 
     return 'Ok\n'
